e060f2b54f5045efd0815af330398474574c686d
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-11-06 15:14:56 +0100

    move sources, update swift4, update spm, update readme

--- Package.swift
+++ Package.swift
@@ -1,7 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "LocalizationToolkit",
-    dependencies: [
+    products: [
+        .library(name: "LocalizationToolkit", targets: ["LocalizationToolkit"])
+    ],
+    targets: [
+        .target(name: "LocalizationToolkit"),
+        .testTarget(
+            name: "LocalizationToolkitTests",
+            dependencies: [
+                "LocalizationToolkit"
+            ]
+        )
     ]
 )
